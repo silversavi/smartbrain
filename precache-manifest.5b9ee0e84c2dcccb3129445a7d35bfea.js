da6369472e30510425929435b58456628e85fa6e
Silver Savi <silver.savi@gmail.com>
2019-09-02 17:26:09 +0300

    Updates

---
+++ precache-manifest.5b9ee0e84c2dcccb3129445a7d35bfea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade9be3fd019d36137240117e5e63d36",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "87681318750f26ac3cd4",
+    "url": "/smartbrain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "0c3e6d9fb91783e42e8c",
+    "url": "/smartbrain/static/css/main.0e6656ba.chunk.css"
+  },
+  {
+    "revision": "87681318750f26ac3cd4",
+    "url": "/smartbrain/static/js/2.eb7b3746.chunk.js"
+  },
+  {
+    "revision": "0c3e6d9fb91783e42e8c",
+    "url": "/smartbrain/static/js/main.c185f932.chunk.js"
+  },
+  {
+    "revision": "9ac85057d7b96f435469",
+    "url": "/smartbrain/static/js/runtime~main.e0aa727b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0fcef41de5c653ed22a5aa5a1c7d4fdb.js"
+  "/smartbrain/precache-manifest.5b9ee0e84c2dcccb3129445a7d35bfea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/smartbrain/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
